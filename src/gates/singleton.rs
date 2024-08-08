da13b7b8790f2737db124c4d5bd7de61b2b309d3
Haadi Khan <haadikhan05@gmail.com>
2024-08-07 21:18:26 -0400

    Switched to nalgebra
    also aliased Complex64 as c64

--- src/gates/singleton.rs
+++ src/gates/singleton.rs
@@ -1,5 +1,5 @@
-use ndarray::Array2;
-use numpy::Complex64;
+use nalgebra::base::Matrix2;
+use numpy::Complex64 as c64;
 
 use crate::operations::{Gate, TimeUnit};
 
@@ -11,84 +11,73 @@ macro_rules! call_all_functions {
 
 pub fn hadamard() -> Gate {
     let factor = 1.0 / 2.0_f64.sqrt();
-    let matrix = Array2::from_shape_vec(
-        (2, 2),
+    let matrix = Matrix2::from_vec(
         vec![
-            Complex64::new(factor, 0.0),
-            Complex64::new(factor, 0.0),
-            Complex64::new(factor, 0.0),
-            Complex64::new(-factor, 0.0),
+            c64::new(factor, 0.0),
+            c64::new(factor, 0.0),
+            c64::new(factor, 0.0),
+            c64::new(-factor, 0.0),
         ],
-    )
-    .unwrap();
+    );
     Gate::new("h".to_string(), vec![], None, TimeUnit::DT, matrix, None)
 }
 
 pub fn x() -> Gate {
-    let matrix = Array2::from_shape_vec(
-        (2, 2),
+    let matrix = Matrix2::from_vec(
         vec![
-            Complex64::new(0.0, 0.0),
-            Complex64::new(1.0, 0.0),
-            Complex64::new(1.0, 0.0),
-            Complex64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(1.0, 0.0),
+            c64::new(1.0, 0.0),
+            c64::new(0.0, 0.0),
         ],
-    )
-    .unwrap();
+    );
     Gate::new("x".to_string(), vec![], None, TimeUnit::DT, matrix, None)
 }
 
 pub fn y() -> Gate {
-    let matrix = Array2::from_shape_vec(
-        (2, 2),
+    let matrix = Matrix2::from_vec(
         vec![
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, -1.0),
-            Complex64::new(0.0, 1.0),
-            Complex64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, -1.0),
+            c64::new(0.0, 1.0),
+            c64::new(0.0, 0.0),
         ],
-    )
-    .unwrap();
+    );
     Gate::new("y".to_string(), vec![], None, TimeUnit::DT, matrix, None)
 }
 
 pub fn z() -> Gate {
-    let matrix = Array2::from_shape_vec(
-        (2, 2),
+    let matrix = Matrix2::from_vec(
         vec![
-            Complex64::new(1.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(-1.0, 0.0),
+            c64::new(1.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(-1.0, 0.0),
         ],
-    )
-    .unwrap();
+    );
     Gate::new("z".to_string(), vec![], None, TimeUnit::DT, matrix, None)
 }
 
 pub fn cx() -> Gate {
-    let matrix = Array2::from_shape_vec(
-        (4, 4),
+    let matrix = Matrix2::from_vec(
         vec![
-            Complex64::new(1.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(1.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(1.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(0.0, 0.0),
-            Complex64::new(1.0, 0.0),
-            Complex64::new(0.0, 0.0),
+            c64::new(1.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(1.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(1.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(0.0, 0.0),
+            c64::new(1.0, 0.0),
+            c64::new(0.0, 0.0),
         ],
-    )
-    .unwrap();
-
+    );
     Gate::new("cx".to_string(), vec![], None, TimeUnit::DT, matrix, None)
 }
